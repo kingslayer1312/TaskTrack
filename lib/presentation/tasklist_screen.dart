e434d69fa1f395f567c0b35508409360a9e80c78
Kingslayer1312 <hrishabari@gmail.com>
2023-12-24 23:57:32 +0530

    fixed skeleton for all screens

---
+++ lib/presentation/tasklist_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TasklistScreen extends StatefulWidget {
+  const TasklistScreen({super.key});
+
+  @override
+  State<TasklistScreen> createState() => _TasklistScreenState();
+}
+
+class _TasklistScreenState extends State<TasklistScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
